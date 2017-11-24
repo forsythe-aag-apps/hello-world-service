16549923603f41d7b8814b4ac2a162c37a80db25
cd-pipeline <33746444+cd-pipeline@users.noreply.github.com>
2017-11-24 15:44:19 +0300

    Added test vulnerability

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -12,7 +12,11 @@ import java.util.Arrays;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        try {
+            SpringApplication.run(Application.class, args);
+        } catch (Exception ex) {
+            throw ex;
+        }
     }
 
     @Bean
